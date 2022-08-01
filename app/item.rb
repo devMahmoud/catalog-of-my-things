ac43a1c5f76f55a8f8024fcbefc38fe87df63823
Rafael Guzman <rega1237@gmail.com>
2022-08-01 16:40:09 -0400

    initialize item class

--- app/item.rb
+++ app/item.rb
@@ -1,3 +1,25 @@
 class Item
+  attr_reader :genre, :author, :source, :label
 
+  def initialize(date, archived: false)
+    @id = Random.rand(1..1000)
+    @publish_date = date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
 end
