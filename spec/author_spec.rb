0331b37ba997a0638840b327fd4f77ce159fc449
Rafael Guzman <rega1237@gmail.com>
2022-08-04 16:53:12 -0400

    add test for author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../app/author.rb'
+require_relative '../app/game.rb'
+
+describe Author do
+  context 'When testing Author class' do
+    before(:all) do
+      @author = Author.new('J.K', 'Rowling')
+      @game = Game.new('yes', 2021, 2000)
+    end
+
+    it 'The Author class initialize correctly' do
+      expect(@author.first_name).to eq 'J.K'
+      expect(@author.last_name).to eq 'Rowling'
+    end
+
+    it 'Add a new item to the Author class and check association one to many with Item class' do
+      @author.add_item(@game)
+      expect(@author.items[0].publish_date).to eq 2000
+      expect(@game.author).to eq @author
+    end
+  end
+end
